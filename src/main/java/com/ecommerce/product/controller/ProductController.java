01836591c56b38c79b7dff93e233c996a8d44132
hockey007 <harikrrishna.nuvvula@gmail.com>
2024-09-22 12:35:13 +0530

    Added a new endpoint to getAll Products

--- src/main/java/com/ecommerce/product/controller/ProductController.java
+++ src/main/java/com/ecommerce/product/controller/ProductController.java
@@ -5,10 +5,9 @@ import com.ecommerce.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -22,4 +21,10 @@ public class ProductController {
         productService.addProduct(product);
         return new ResponseEntity<>("Successfully created the product", HttpStatus.ACCEPTED);
     }
+
+    @GetMapping("/getAll")
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
+
 }
